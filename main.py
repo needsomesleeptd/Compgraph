9dee2b65d43f5f8b7703af670ae123d0a3a231c1
Andrew Razin <razin.andrew.ru@mail.ru>
2023-02-19 17:36:32 +0300

    added widgets

--- main.py
+++ main.py
@@ -1,25 +1,34 @@
 import sys
-from PyQt5 import QtWidgets, QtGui, QtCore, uic
+from PyQt5 import QtWidgets, QtGui, QtCore
 
 
-
-
-class Canvas(QtWidgets.QMainWindow):
+class Table(QtWidgets.QTableWidget):
     def __init__(self):
         super().__init__()
-        #uic.loadUi('gui.ui', self)
-        self.setFixedSize(self.size())
-        self.show()
+        self.setColumnCount(2)
+        self.setHorizontalHeaderLabels(["x", "y"])
+
+
+
+class Canvas(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
         self.points = QtGui.QPolygon()
+        canvas_layout = QtWidgets.QGridLayout()
+        frame = QtWidgets.QFrame()
+        frame.setStyleSheet("border: 4px solid rgb(0, 0, 0);")
+        frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        canvas_layout.addWidget(frame)
+        self.setLayout(canvas_layout)
+
+
 
     def mousePressEvent(self, e):
         self.points << e.pos()
         self.update()
 
     def paintEvent(self, ev):
-
         qp = QtGui.QPainter(self)
-
         qp.setRenderHint(QtGui.QPainter.Antialiasing)
         pen = QtGui.QPen(QtCore.Qt.blue, 3)
         brush = QtGui.QBrush(QtCore.Qt.red)
@@ -28,7 +37,30 @@ class Canvas(QtWidgets.QMainWindow):
         qp.drawPoints(self.points)
 
 
+
+class UI(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        layout = QtWidgets.QGridLayout()
+        self.canvas = Canvas()
+        self.label = QtWidgets.QLabel()
+
+        self.table = Table()
+        layout.addWidget(self.canvas,0,0)
+        layout.addWidget(self.table, 0,1,int(self.height() / 2),9)
+        container = QtWidgets.QWidget()
+
+
+
+        container.setLayout(layout)
+        container.resize(200, 100)
+        self.setCentralWidget(container)
+
+        self.update()
+        self.show()
+
+
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
-    window = Canvas()
+    window = UI()
     sys.exit(app.exec())
