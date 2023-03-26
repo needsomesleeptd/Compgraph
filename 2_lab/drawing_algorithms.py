dc6f0a0e7d9cb8f134ac2bbcc475842f3862f867
Andrew Razin <razin.andrew.ru@mail.ru>
2023-03-26 23:19:07 +0300

    added drawing algorithms fixes

--- 2_lab/drawing_algorithms.py
+++ 2_lab/drawing_algorithms.py
@@ -49,28 +49,6 @@ def get_spectre_coords(line_len, point_center, min_angle_diff):
     return spectre_coords
 
 
-'''def bresenhamAlogorithmFloat(xFr:int,yFr:int,xTo:int,yTo:int):
-	points = QPolygonF()
-	deltaX =  abs(xFr - xTo)
-	deltaY = abs(yFr - yTo)
-	error = 0
-	deltaError = (deltaY + 1) / (deltaX + 1)
-	y = yFr
-	dirY = yTo - yFr
-	if dirY > 0:
-		dirY = 1
-	if dirY < 0:
-		dirY = -1
-		
-	
-	for x in range(xFr,yTo + 1):
-		point = QPoint(x,y)
-		points.append(point)
-		error = error + deltaError
-		if error >= 1.0:
-			y = y + dirY
-			error = error - 1.0
-	return points'''
 
 
 def bresenhamAlogorithmFloat(xFr: float, yFr: float, xTo: float, yTo: float):
