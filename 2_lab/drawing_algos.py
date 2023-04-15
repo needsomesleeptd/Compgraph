1b78065260ee8b1f67ca358ef9b01faa451796f2
Andrew Razin <razin.andrew.ru@mail.ru>
2023-04-15 21:39:20 +0300

    added comments + fixed algo

--- 2_lab/drawing_algos.py
+++ 2_lab/drawing_algos.py
@@ -63,6 +63,42 @@ def get_pixel_color(canvas, x, y):
     return QColor(canvas.image.pixel(x, y))
 
 
+def check_line(canvas, stack, cur_pixel, xr, fill_colour, border_colour, dy):
+    x = cur_pixel[0]
+    y = cur_pixel[1]
+    y = y + dy
+    while x <= xr:
+        flag = False # Флаг показыващий что мы нашли пиксель для добавления в стэк
+
+        while get_pixel_color(canvas, x, y) != fill_colour and \
+                get_pixel_color(canvas, x, y) != border_colour and x <= xr: #Идем пока наш интервал незакрашенных пикселей не прерван
+            flag = True
+            x += 1
+
+        # Помещаем в стек крайний справа пиксель
+
+        if flag:# Нашли правый незакрашенный пиксель
+            if x == xr and get_pixel_color(canvas, x, y) != fill_colour and \
+                    get_pixel_color(canvas, x, y) != border_colour:
+                if y < canvas.image.height(): # Если и есть наш пиксель (самый правый то берем его)
+                    stack.append([x, y])
+            else:
+                if y < canvas.image.height():
+                    stack.append([x - 1, y]) #Иначе берем пиксель левее его(наш флаг говорит о том что он точно есть)
+
+            flag = False
+
+        # Продолжаем проверку, если интервал был прерван
+
+        x_in = x
+        while (get_pixel_color(canvas, x, y) == fill_colour or
+               get_pixel_color(canvas, x, y) == border_colour) and x < xr: # Идем направо по пикселям которые
+            x = x + 1                                                      #Прерывают незакрашенные
+
+        if x == x_in: #Проверяем что координата пикселя точно увеличена
+            x += 1
+
+
 def line_by_line_filling_algorithm_with_seed(canvas, border_colour, fill_colour, seed_point, delay=0):
     stack = [seed_point]
     while stack:
@@ -72,24 +108,22 @@ def line_by_line_filling_algorithm_with_seed(canvas, border_colour, fill_colour,
         y = seed_pixel[1]
 
         canvas.image.setPixelColor(x, y, fill_colour)
-        tx = x
-        ty = y
+        save_x = x
+        save_y = y
 
-        # заполняем интервал справа от затравки
-
+        # Заполняем интервал справа от затравки
         x += 1
 
         while get_pixel_color(canvas, x, y) != fill_colour and \
                 get_pixel_color(canvas, x, y) != border_colour and x < canvas.image.width():
             canvas.image.setPixelColor(x, y, fill_colour)
             x += 1
-        # print(get_pixel_color(canvas,x,y).Rgb ,fill_colour.Rgb)
 
         xr = x - 1
 
-        # заполняем интервал слева от затравки
+        #Заполняем интервал слева от затравки
 
-        x = tx - 1
+        x = save_x - 1
         while get_pixel_color(canvas, x, y) != fill_colour and \
                 get_pixel_color(canvas, x, y) != border_colour and x > 0:
             canvas.image.setPixelColor(x, y, fill_colour)
@@ -97,79 +131,13 @@ def line_by_line_filling_algorithm_with_seed(canvas, border_colour, fill_colour,
 
         xl = x + 1
 
-        # Проход по верхней строке
 
-        x = xl
-        if ty < canvas.image.height():
-            y = ty + 1
 
-            while x <= xr:
-                flag = False
 
-                while get_pixel_color(canvas, x, y) != fill_colour and \
-                        get_pixel_color(canvas, x, y) != border_colour and x <= xr:
-                    flag = True
-                    x += 1
-
-                # Помещаем в стек крайний справа пиксель
-
-                if flag:
-                    if x == xr and get_pixel_color(canvas, x, y) != fill_colour and \
-                            get_pixel_color(canvas, x, y) != border_colour:
-                        if y < canvas.image.height():
-                            stack.append([x, y])
-                    else:
-                        if y < canvas.image.height():
-                            stack.append([x - 1, y])
-
-                    flag = False
-
-                # Продолжаем проверку, если интервал был прерван
-
-                x_in = x
-                while (get_pixel_color(canvas, x, y) == fill_colour or
-                       get_pixel_color(canvas, x, y) == border_colour) and x < xr:
-                    x = x + 1
-
-                if x == x_in:
-                    x += 1
-
-        # Проход по нижней строке
-
-        x = xl
-        y = ty - 1
-
-        while x <= xr:
-            flag = False
-
-            while get_pixel_color(canvas, x, y) != fill_colour and \
-                    get_pixel_color(canvas, x, y) != border_colour and x <= xr:
-                flag = True
-                x += 1
-
-            # Помещаем в стек крайний справа пиксель
-
-            if flag:
-
-                if x == xr and get_pixel_color(canvas, x, y) != fill_colour and \
-                        get_pixel_color(canvas, x, y) != border_colour:
-                    if y > 0:
-                        stack.append([x, y])
-                else:
-                    if y > 0:
-                        stack.append([x - 1, y])
-
-                flag = False
-
-            # Продолжаем проверку, если интервал был прерван
-
-            x_in = x
-            while (get_pixel_color(canvas, x, y) == fill_colour or
-                   get_pixel_color(canvas, x, y) == border_colour) and x < xr:
-                x = x + 1
-
-            if x == x_in:
-                x += 1
+        if save_y < canvas.image.height():
+            check_line(canvas, stack, [xl, save_y], xr, fill_colour, border_colour, 1) #Проходим по строке снизу y = y +1
+        if (save_y > 0):
+            check_line(canvas, stack, [xl, save_y], xr, fill_colour, border_colour, -1)#Проходим по строке сверху y = y - 1
 
         if delay != 0:
             QtTest.QTest.qWait(delay)
@@ -252,31 +220,29 @@ def get_intersections(edges):
                 canvas.image.setPixelColor(k, y_inter, fill_color)
 '''
 
-def rastr_algo_flag(canvas,fill_color, background_color, polygons):
+
+def rastr_algo_flag(canvas, fill_color, background_color, polygons):
     if (len(polygons) == 0):
         min_y = 0
         min_x = 0
         max_x = canvas.image.width()
         max_y = canvas.image.height()
     else:
-        min_y = int(apply_to_dots(polygons, lambda tar,dots:min(tar,dots[1])))
-        max_y = ceil(apply_to_dots(polygons, lambda tar,dots:max(tar,dots[1])))
+        min_y = int(apply_to_dots(polygons, lambda tar, dots: min(tar, dots[1])))
+        max_y = ceil(apply_to_dots(polygons, lambda tar, dots: max(tar, dots[1])))
 
         min_x = int(apply_to_dots(polygons, lambda tar, dots: min(tar, dots[0])))
         max_x = ceil(apply_to_dots(polygons, lambda tar, dots: max(tar, dots[0])))
 
-
-    for y in range(min_y, max_y):
+    for y in range(min_y + 1, max_y - 1):
         flag = False
 
-        for x in range(min_x, max_x + 2):
+        for x in range(min_x, max_x + 1):
 
-            if (get_pixel_color(canvas,x, y) == canvas.pen.color()):
+            if (get_pixel_color(canvas, x, y) == canvas.pen.color()):
                 flag = not flag
 
             if flag:
-                canvas.image.setPixelColor(x,y,fill_color)
+                canvas.image.setPixelColor(x, y, fill_color)
             else:
                 canvas.image.setPixelColor(x, y, background_color)
-
-
